df026178619e5e5ad8fad59c21322bf334f5706a
Juan Ochoa <juanoc@microsoft.com>
2018-06-13 18:46:18 -0700

    Custom nodejs game

--- server.js
+++ server.js
@@ -1,43 +1,54 @@
 var express = require('express');
 var http = require('http');
-var socketio = require('socket.io');
-var Game = require('./lib/game');
-var options = require('./public/options.json');
+var path = require('path');
+var socketIO = require('socket.io');
+
+// Constants
+const portNumber = 8888;
+
 var app = express();
-
-app.use(express.static('public'));
-app.get('/', function (request, response) {
-    response.send('Hello World');
-});
-
 var server = http.Server(app);
-var io = socketio(server);
-var port = process.env.port;
-server.listen(port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+var io = new socketIO(server);
 
-    console.log('Listening on http://%s:%s', host, port);
+app.set('port', portNumber);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Set routing
+app.get('/', function(request, response)
+{
+    response.sendFile(path.join(__dirname, "index.html"));
 });
 
-io.on('connection', function (socket) {
-    console.log('%s connected', socket.id);
-    game.addPlayer(socket.id);
-
-    socket.on('disconnect', function () {
-        console.log('%s disconnected', socket.id);
-        game.removePlayer(socket.id);
-    });
-
-    socket.on('position', function (data) {
-        console.log('%s is at (%s, %s)', socket.id, data.x, data.y);
-        game.update(socket.id, data);
-    });
+server.listen(portNumber, function() {
+    console.log('Started listening on port: ' + portNumber);
 });
 
-var game = new Game(options);
-function emitGameState() {
-    game.tick();
-    io.emit('state', game.state());
-};
-setInterval(emitGameState, 1000 / 60);
+var players = {};
+io.on('connection', function(socket) {
+  console.log("Connection just happened");
+  socket.on('new player', function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300
+    };
+  });
+  socket.on('movement', function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
+});
+
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000 / 60);
